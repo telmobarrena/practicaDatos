a8ee1b7d905cfb90e18563bef0f7a011d59b428a
nicolasserrano <nicolas.serrano.barcena@gmail.com>
2018-09-15 17:45:54 +0200

    Update leerPoblaciones.js

--- geocoordenadas/leerPoblaciones.js
+++ geocoordenadas/leerPoblaciones.js
@@ -1,13 +1,15 @@
 function leerPoblaciones() {
     villagesTR = document.getElementsByTagName('table')[0].getElementsByTagName('tr')
-    str='';
+    str='[';
     for (i=4;i<villagesTR.length;i++) {
       long = villagesTR[i].children[4].textContent.replace(',','.');
-      lat  = villagesTR[i].children[3].textContent.replace(',','.')
+      lat  = villagesTR[i].children[3].textContent.replace(',','.');
+      hab = villagesTR[i].children[6].textContent;
       str += "[" + long
       str += "," + lat
-      str += ",'" + villagesTR[i].children[2].textContent.replace("'"," ").replace("'"," ").replace("\n","") + " " + long + " " + lat + "'],"
+      str += ",'" + villagesTR[i].children[2].textContent.replace("'"," ").replace("'"," ").replace("\n","") + " " + long + " " + lat + "', " + hab + "],"
       str += "\n"
     }
+    str += "[,,]]"
     return str;
 }
